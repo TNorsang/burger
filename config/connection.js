d47f0a2f839ce74ba1b8ae8ca26fef05161d23db
git config --global user.name TNorsang <thenorsang@gmail.com>
2019-03-10 21:14:57 -0400

    added connection schemaa seeds logo handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-// Set up database connection
+// database connected
 var mysql = require('mysql');
 var connection;
 
@@ -9,22 +9,11 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
 		host: 'localhost',
 		user: 'root',
-		password: '',
+		password: 'root',
 		database: 'burgers_db'
 	});
 };
 
-
-// var connection = mysql.createConnection({
-// 	host: "localhost",
-// 	port: 3306,
-// 	user: "root",
-// 	password: "",
-// 	database: "burgers_db" 
-// });
-
-// not strictly required, but best practice to establish connection
-// it's possible query establishes the connection
 connection.connect(function(err) {
 	if (err) {
 		console.error('error conencting: ' + err.stack);
